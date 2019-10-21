23712d966b97c581f517df04ccc212871ec6f8bb
jayisaac0 <jayisaac0@gmail.com>
2019-10-21 14:33:19 +0300

    p api

--- routes/categories.js
+++ routes/categories.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const connection = mysql.createConnection({host : process.env.DATABASE_HOST, user : process.env.DATABASE_USER, password : process.env.DATABASE_PASSWORD, database : process.env.DATABASE_NAME});
 connection.connect((error) => {
-    if (error) throw error
+    if (error) throw error;
     console.log('CONNECTED');
 });
 
